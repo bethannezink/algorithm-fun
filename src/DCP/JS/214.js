43a3ff895fb70394da1ca2e15dfdca14935bc719
Bethanne Zink <bethannezink@gmail.com>
2018-08-05 16:45:34 -0400

    add problem 214

---
+++ src/DCP/JS/214.js
@@ -0,0 +1,44 @@
+String.prototype.reverse = function() {
+	let reversedString = '';
+
+	for (let i = this.length - 1; i >= 0; i--) {
+		reversedString += this.charAt(i);
+	}
+
+	return reversedString;
+}
+
+function convertToBinary(decimalInput, binaryString = '') {
+	binaryString += decimalInput % 2;
+	decimalInput = Math.floor(decimalInput / 2);
+
+	if (decimalInput > 0) {
+		return convertToBinary(decimalInput, binaryString);
+	}
+
+	return binaryString.reverse();
+}
+
+function getLongestSequence(number) {
+	let binaryString = convertToBinary(number);
+	let longestSequence = 1, counter = 1;
+
+	for (let i = 0; i < binaryString.length; i++) {
+		let currentValue = binaryString.charAt(i);
+		let nextValue = binaryString.charAt(i + 1);
+
+		if (currentValue === nextValue) {
+			counter++;
+
+			if (counter > longestSequence) {
+				longestSequence = counter;
+			}
+
+		} else {
+			counter = 1;
+		}
+
+	}
+
+	return longestSequence;
+}

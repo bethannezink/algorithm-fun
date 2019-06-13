1882f518b79dfcea8bb728af92a71402b4c3e5a1
Bethanne Zink <bethannezink@gmail.com>
2019-06-13 09:17:15 -0400

    add considerations and progress on word cloud data problem

--- src/interviewCake/wordCloudData/wordCloudData.js
+++ src/interviewCake/wordCloudData/wordCloudData.js
@@ -6,22 +6,25 @@
 // What do we want to do with "After", "Dana", and "add"? In this example, your final dictionary should include one "Add" or "add" with a value of 22. Make reasonable (not necessarily perfect) decisions about cases like "After" and "Dana".
 // Assume the input will only contain words and standard punctuation.
 
+// lowercase all words -->
+// consider punctuation -- look at charcode to see if it's a letter?
+
 const validLetterCharCodes = []
 
-// function buildWordCloud (wordString) {
-//   const wordArray = wordString.split(' ').join();
-//   const wordCloud = {};
+function buildWordCloud (wordString) {
+  const wordArray = wordString.split(' ').join();
+  const wordCloud = {};
 
-//   for (let i = 0; i < wordArray; i++) {
-//     for (let j = 0; j < wordArray[i].length; j++) {
-//       const wordToAdd = '';
-//       if ((wordArray[i].charCodeAt(j) > 64 && wordArray[i].charCodeAt(j) < 91) || (wordArray[i].charCodeAt(j) > 96 && wordArray[i].charCodeAt(j) < 123)) {
-//         wordToAdd += wordArray[i].charCodeAt(j);
-//       }
-//     }
+  for (let i = 0; i < wordArray; i++) {
+    for (let j = 0; j < wordArray[i].length; j++) {
+      const wordToCheck = wordArry[i].toLowerCase();
+      let wordToAddToCloud = '';
+
+      if (wordToCheck.charCodeAt(j) >= 97 && wordToCheck.charCodeAt(j) <= 122) {
+        wordToAdd += wordToCheck.charCodeAt(j);
+      }
+    }
     
-//     wordCloud 
-//   }
-
-//   if 
-// }
+    wordCloud 
+  }
+}

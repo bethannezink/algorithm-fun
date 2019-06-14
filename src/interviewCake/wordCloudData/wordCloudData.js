2ecb930e670b282dc8e83346f9153d8f99c379ea
Bethanne Zink <bethannezink@gmail.com>
2019-06-14 08:26:49 -0400

    revise to only loop once

--- src/interviewCake/wordCloudData/wordCloudData.js
+++ src/interviewCake/wordCloudData/wordCloudData.js
@@ -6,27 +6,27 @@
 // What do we want to do with "After", "Dana", and "add"? In this example, your final dictionary should include one "Add" or "add" with a value of 22. Make reasonable (not necessarily perfect) decisions about cases like "After" and "Dana".
 // Assume the input will only contain words and standard punctuation.
 
+// To do: revise to only iterate over string one time
 function buildWordCloud (wordString) {
-  const wordArray = wordString.split(' ');
+  wordString = wordString.toLowerCase();
   const wordCloud = {};
+  let wordToAddToCloud = '';
 
-  for (let i = 0; i < wordArray.length; i++) {
-    let wordToAddToCloud = '';
+  for (let i = 0; i < wordString.length; i++) {
+    const isHyphen = wordString.charCodeAt(i) === 45;
+    const isLetter = wordString.charCodeAt(i) >= 97 && wordString.charCodeAt(i) <= 122;
+    const isSpace = wordString.charCodeAt(i) === 32;
+    const isLastCharacter = i === wordString.length - 1;
 
-    for (let j = 0; j < wordArray[i].length; j++) {
-      const wordToCheck = wordArray[i].toLowerCase();
-      const isHyphen = wordToCheck.charCodeAt(j) === 45;
-
-      // should I strictly look at beginning (") and end of word (,"-?!)
-      if (wordToCheck.charCodeAt(j) >= 97 && wordToCheck.charCodeAt(j) <= 122 || isHyphen) {
-        wordToAddToCloud += wordToCheck.charAt(j);
+    if (isLetter || isHyphen) {
+      wordToAddToCloud += wordString.charAt(i);
+    } else if (isSpace || isLastCharacter) {
+      if (wordCloud[wordToAddToCloud]) {
+        wordCloud[wordToAddToCloud]++;
+      } else {
+        wordCloud[wordToAddToCloud] = 1;
       }
-    }
-
-    if (wordCloud[wordToAddToCloud]) {
-      wordCloud[wordToAddToCloud]++;
-    } else {
-      wordCloud[wordToAddToCloud] = 1;
+      wordToAddToCloud = '';
     }
   }
 
--- src/interviewCake/wordCloudData/wordCloudData.test.js
+++ src/interviewCake/wordCloudData/wordCloudData.test.js
@@ -1,25 +1,30 @@
 const { buildWordCloud } = require('./wordCloudData');
 
-const words = 'After beating the eggs, Dana read the next step: Add milk and eggs, then add flour and sugar.'
+test('builds a word cloud for a word string with basic punctuation and few relevant proper nouns', () => {
+  const wordString = 'After beating the eggs, Dana read the next step: Add milk and eggs, then add flour and sugar.'
 
-const expectedWordCloud = {
-  after: 1,
-  beating: 1,
-  the: 2,
-  eggs: 1,
-  dana: 1,
-  read: 1,
-  next: 1,
-  step: 1,
-  add: 2,
-  milk: 1,
-  and: 2,
-  eggs: 2,
-  then: 1,
-  flour: 1,
-  sugar: 1
-}
+  const expectedWordCloud = {
+    after: 1,
+    beating: 1,
+    the: 2,
+    eggs: 1,
+    dana: 1,
+    read: 1,
+    next: 1,
+    step: 1,
+    add: 2,
+    milk: 1,
+    and: 2,
+    eggs: 2,
+    then: 1,
+    flour: 1,
+    sugar: 1
+  };
 
-test('test test', () => {
-  expect(buildWordCloud(words)).toMatchObject(expectedWordCloud);
+  expect(buildWordCloud(wordString)).toMatchObject(expectedWordCloud);
 });
+
+test('', () => {
+  const wordString = "We came, we saw, we conquered...then we ate Bill's (Mille-Feuille) cake. The bill came to five dollars."
+  // expect(buildWordCloud(wordString)).toMatchObject(expectedWordCloud);
+})

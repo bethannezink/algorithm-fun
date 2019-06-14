37e7734d4e5645cce64a83f354e2e9ea232c01d2
Bethanne Zink <bethannezink@gmail.com>
2019-06-14 08:29:02 -0400

    remove to do

--- src/interviewCake/wordCloudData/wordCloudData.js
+++ src/interviewCake/wordCloudData/wordCloudData.js
@@ -6,7 +6,6 @@
 // What do we want to do with "After", "Dana", and "add"? In this example, your final dictionary should include one "Add" or "add" with a value of 22. Make reasonable (not necessarily perfect) decisions about cases like "After" and "Dana".
 // Assume the input will only contain words and standard punctuation.
 
-// To do: revise to only iterate over string one time
 function buildWordCloud (wordString) {
   wordString = wordString.toLowerCase();
   const wordCloud = {};

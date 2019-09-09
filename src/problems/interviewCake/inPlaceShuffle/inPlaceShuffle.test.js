cbcfb25dee7e0ea118a3e201b416632266971333
Bethanne Zink <bethannezink@gmail.com>
2019-09-08 23:15:16 -0400

    add test

--- src/problems/interviewCake/inPlaceShuffle/inPlaceShuffle.test.js
+++ src/problems/interviewCake/inPlaceShuffle/inPlaceShuffle.test.js
@@ -1,6 +1,8 @@
 const { shuffleArray } = require('./inPlaceShuffle');
 
 test('', () => {
-  // const sortedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // expect(shuffleArray(sortedArray)).toBe();
+  const sortedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const shuffledArray = shuffleArray(sortedArray);
+  console.log(sortedArray,shuffledArray);
+  expect(sortedArray).not.toMatchObject(shuffledArray);
 });

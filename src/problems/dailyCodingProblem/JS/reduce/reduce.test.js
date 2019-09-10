12331510617c922f1db3b01545d4979a4cbc5ae9
Bethanne Zink <bethannezink@gmail.com>
2019-09-10 07:37:26 -0400

    add reduce tests

--- src/problems/dailyCodingProblem/JS/reduce/reduce.test.js
+++ src/problems/dailyCodingProblem/JS/reduce/reduce.test.js
@@ -1,33 +1,37 @@
 const { reduce } = require('./reduce');
 
-test('', () => {
-  function sum (a, b) {
-    return a + b;
-  }
-  
-  expect(reduce([1, 2, 3], sum, 0)).toEqual(6);
+function sum (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+test('returns the initial value when the array is emptys', () => {
+  expect(reduce([], sum, 0)).toEqual(0);
 });
 
-test('', () => {
-  function subtract (a, b) {
-    return a - b;
-  }
-  
-  expect(reduce([1, 2, 3], subtract, 0)).toEqual(-6);
+test('subtracts the contents of an array when a sum function is passed', () => {
+  expect(reduce([1, 2, 3], sum, 0)).toEqual(1 + 2 + 3);
 });
 
-test('', () => {
-  function multiply (a, b) {
-    return a * b;
-  }
-  
-  expect(reduce([1, 2, 3], multiply, 1)).toEqual(6);
+test('subtracts the contents of an array when a subtract function is passed', () => {
+  expect(reduce([1, 2, 3], subtract, 0)).toEqual(0 - 1 - 2 - 3);
 });
 
-test('', () => {
-  function multiply (a, b) {
-    return a * b;
-  }
-  
-  expect(reduce([1, 2, 3], multiply, 0)).toEqual(0);
+test('multiplies the contents of an array when a multiply function is passed', () => {
+  expect(reduce([1, 2, 3], multiply, 1)).toEqual(1 * 2 * 3);
+});
+
+test('divides the contents of an array when a divide function is passed', () => {
+  expect(reduce([1, 2, 3], divide, 1)).toEqual(1 / 2 / 3);
 });

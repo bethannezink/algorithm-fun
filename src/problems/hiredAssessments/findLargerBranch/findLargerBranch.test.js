93b8cb0e508b68f46a707c82fc2e52dd57ce478f
Bethanne Zink <bethannezink@gmail.com>
2020-01-01 11:54:07 -0500

    comment tests back in

--- src/problems/hiredAssessments/findLargerBranch/findLargerBranch.test.js
+++ src/problems/hiredAssessments/findLargerBranch/findLargerBranch.test.js
@@ -1,17 +1,17 @@
 const { findLargerBranch } = require('./findLargerBranch');
 
-// test('handles a tree with a larger left branch', () => {
-//   expect(findLargerBranch([3,6,2,9,-1,10])).toEqual('Left');
-// });
+test('handles a tree with a larger left branch', () => {
+  expect(findLargerBranch([3,6,2,9,-1,10])).toEqual('Left');
+});
 
 test('handles a tree with a larger right branch', () => {
   expect(findLargerBranch([1,4,100,5])).toEqual('Right');
 });
 
-// test('handles a tree with equal branches', () => {
-//   expect(findLargerBranch([1,10,5,1,0,6])).toEqual('');
-// });
+test('handles a tree with equal branches', () => {
+  expect(findLargerBranch([1,10,5,1,0,6])).toEqual('');
+});
 
-// test('handles a tree with no nodes', () => {
-//   expect(findLargerBranch([])).toEqual('');
-// });
+test('handles a tree with no nodes', () => {
+  expect(findLargerBranch([])).toEqual('');
+});

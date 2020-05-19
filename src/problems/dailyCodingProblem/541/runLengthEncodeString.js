45f7405c7e9a0ebf75ec73d0a87c08b06b981817
Bethanne Zink <bethannezink@gmail.com>
2020-05-19 10:06:42 -0400

    Add working solution for runLengthDecodeString for single-digit numbers only

--- src/problems/dailyCodingProblem/541/runLengthEncodeString.js
+++ src/problems/dailyCodingProblem/541/runLengthEncodeString.js
@@ -6,6 +6,8 @@
 // You can assume the string to be encoded have no digits and consists solely of alphabetic characters.
 // You can assume the string to be decoded is valid.
 
+// Big O: O(n)
+
 function runLengthEncodeString(string) {
   let encodedString = '';
   let currentLetter = string.charAt(string.length - 1);
@@ -16,17 +18,31 @@ function runLengthEncodeString(string) {
 
     if (nextLetter === currentLetter) {
       currentLetterCount++;
-
-      if (i === 0) {
-        return currentLetterCount.toString() + currentLetter + encodedString;
-      }
-
     } else if (nextLetter !== currentLetter) {
       encodedString = currentLetterCount.toString() + currentLetter + encodedString;
       currentLetter = nextLetter;
       currentLetterCount = 1;
     }
+
+    if (i === 0) {
+      return currentLetterCount.toString() + currentLetter + encodedString;
+    }
   }
 }
 
-module.exports = { runLengthEncodeString };
+function runLengthDecodeString(string) {
+  let decodedString = '';
+  for (let i = 0; i < string.length; i += 2) {
+    let numCurrentLetter = parseInt(string.charAt(i), 10);
+    const currentLetter = string.charAt(i + 1);
+
+    while (numCurrentLetter > 0) {
+      decodedString += currentLetter;
+      numCurrentLetter--;
+    }
+  }
+
+  return decodedString;
+}
+
+module.exports = { runLengthEncodeString, runLengthDecodeString };
--- src/problems/dailyCodingProblem/541/runLengthEncodeString.test.js
+++ src/problems/dailyCodingProblem/541/runLengthEncodeString.test.js
@@ -1,5 +1,15 @@
-const { runLengthEncodeString } = require('./runLengthEncodeString');
+const { runLengthDecodeString, runLengthEncodeString } = require('./runLengthEncodeString');
 
-test('', () => {
+test('run-length encodes the string', () => {
   expect(runLengthEncodeString('AAAABBBCCDAA')).toEqual('4A3B2C1D2A');
+  expect(runLengthEncodeString('FFFFFJJJJJ')).toEqual('5F5J');
+  expect(runLengthEncodeString('IUACB')).toEqual('1I1U1A1C1B');
+  expect(runLengthEncodeString('YYYYYYYYY')).toEqual('9Y');
+});
+
+test('decodes the run-length encoded string', () => {
+  expect(runLengthDecodeString('4A3B2C1D2A')).toEqual('AAAABBBCCDAA');
+  expect(runLengthDecodeString('5F5J')).toEqual('FFFFFJJJJJ');
+  expect(runLengthDecodeString('1I1U1A1C1B')).toEqual('IUACB');
+  expect(runLengthDecodeString('9Y')).toEqual('YYYYYYYYY');
 });

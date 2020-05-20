91139d7c7f95eea9d8c2ecfa872598b1c93af8f8
Bethanne Zink <bethannezink@gmail.com>
2020-05-20 10:20:36 -0400

    Alter test case to include double-digit numbers

--- src/problems/dailyCodingProblem/541/runLengthEncodeString.test.js
+++ src/problems/dailyCodingProblem/541/runLengthEncodeString.test.js
@@ -4,7 +4,7 @@ test('run-length encodes the string', () => {
   expect(runLengthEncodeString('AAAABBBCCDAA')).toEqual('4A3B2C1D2A');
   expect(runLengthEncodeString('FFFFFJJJJJ')).toEqual('5F5J');
   expect(runLengthEncodeString('IUACB')).toEqual('1I1U1A1C1B');
-  expect(runLengthEncodeString('YYYYYYYYY')).toEqual('9Y');
+  expect(runLengthEncodeString('YYYYYYYYYYYYY')).toEqual('13Y');
 });
 
 test('decodes the run-length encoded string', () => {

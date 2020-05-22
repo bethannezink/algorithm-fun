ab70aec5e286bbc96a2ed53a867c1671b7b65770
Bethanne Zink <bethannezink@gmail.com>
2020-05-22 10:36:13 -0400

    Add slightly-optimized naive solution to findPythagoreanTriplet

--- src/problems/dailyCodingProblem/282/naive/findPythagoreanTriplet.js
+++ src/problems/dailyCodingProblem/282/naive/findPythagoreanTriplet.js
@@ -22,4 +22,28 @@ function findPythagoreanTriplet(array) {
   return false;
 }
 
-module.exports = { findPythagoreanTriplet };
+function findPythagoreanTripletWithSlightOptimization(array) {
+  const squaredArray = array.map(num => num ** 2);
+
+  for (let i = 0; i < squaredArray.length; i++) {
+    for (let j = i + 1; j < squaredArray.length; j++) {
+      for (let k = j + 1; k < squaredArray.length; k++) {
+        const square1 = squaredArray[i];
+        const square2 = squaredArray[j];
+        const square3 = squaredArray[k];
+
+        const isTriplet =
+          square1 + square2 === square3 ||
+          square1 + square3 === square2 ||
+          square2 + square3 === square1;
+
+          if (isTriplet) {
+            return true;
+          }
+      }
+    }
+  }
+  return false;
+}
+
+module.exports = { findPythagoreanTriplet, findPythagoreanTripletWithSlightOptimization };
--- src/problems/dailyCodingProblem/282/naive/findPythagoreanTriplet.test.js
+++ src/problems/dailyCodingProblem/282/naive/findPythagoreanTriplet.test.js
@@ -1,6 +1,10 @@
-const { findPythagoreanTriplet } = require('./findPythagoreanTriplet');
+const { findPythagoreanTriplet, findPythagoreanTripletWithSlightOptimization } = require('./findPythagoreanTriplet');
 
 test('', () => {
   expect(findPythagoreanTriplet([2, 16, 3, 4, 5, 8])).toEqual(true);
   expect(findPythagoreanTriplet([3, 1, 3, 9])).toEqual(false);
+});
+test('', () => {
+  expect(findPythagoreanTripletWithSlightOptimization([2, 16, 3, 4, 5, 8])).toEqual(true);
+  expect(findPythagoreanTripletWithSlightOptimization([3, 1, 3, 9])).toEqual(false);
 });

1e638b17153068fe042d8dc4ac0064e9cb41acbf
Bethanne Zink <bethannezink@gmail.com>
2020-05-30 22:53:46 -0400

    Begin optimal solution of findLargestInclusiveRange

--- src/problems/dailyCodingProblem/393/optimal/findLargestInclusiveRange.js
+++ src/problems/dailyCodingProblem/393/optimal/findLargestInclusiveRange.js
@@ -4,15 +4,37 @@
 // For example, given the array [9, 6, 1, 3, 8, 10, 12, 11], return (8, 12) since 8, 9, 10, 11, and 12
 // are all in the array.
 
+// longest_range = (0, 0)
+// for num in nums:
+// if num - 1 not in nums:
+// curr = num + 1
+// while curr in nums:
+// curr += 1
+// if curr - num > longest_range[1] - longest_range[0]:
+// longest_range = (num, curr - 1)
+// return longest_range
+
 function findLargestInclusiveRange(array) {
   let numberTracker = {};
 
-  for (let num in array) {
-    numberTracker[num] = true;
+  for (let arrayNum of array) {
+    numberTracker[arrayNum] = true;
   }
 
-  let currentRange = 0;
+  let current;
   let longestRange = 0;
+
+  for (let num in numberTracker) {
+
+    if (!numberTracker[num - 1]) {
+      current = num + 1;
+
+      while(numberTracker[current]) {
+        current++;
+      }
+    }
+
+  }
 }
 
 module.exports = { findLargestInclusiveRange };

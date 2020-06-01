1fd0f60667b9bffa754b6e571820162ef8341641
Bethanne Zink <bethannezink@gmail.com>
2020-05-31 22:12:26 -0400

    Clean up variable names for clarity in findLargestInclusiveRange

--- src/problems/dailyCodingProblem/393/optimal/findLargestInclusiveRange.js
+++ src/problems/dailyCodingProblem/393/optimal/findLargestInclusiveRange.js
@@ -4,39 +4,33 @@
 // For example, given the array [9, 6, 1, 3, 8, 10, 12, 11], return (8, 12) since 8, 9, 10, 11, and 12
 // are all in the array.
 
-// longest_range = (0, 0)
-// for num in nums:
-// if num - 1 not in nums:
-// curr = num + 1
-// while curr in nums:
-// curr += 1
-// if curr - num > longest_range[1] - longest_range[0]:
-// longest_range = (num, curr - 1)
-// return longest_range
+// Big O: O(n)
 
 function findLargestInclusiveRange(array) {
-  let numberTracker = {};
+  let tracker = {};
 
-  for (let arrayNum of array) {
-    numberTracker[arrayNum] = arrayNum;
+  for (let numberInArray of array) {
+    tracker[numberInArray] = numberInArray;
   }
 
-  let current;
   let longestRange = [0, 0];
 
-  for (let num in numberTracker) {
+  for (let number in tracker) {
+    const currentNumber = tracker[number];
+    let lastInRange;
+    const isFirstInRange = !tracker[number - 1];
 
-    if (!numberTracker[num - 1]) {
-      current = numberTracker[num] + 1;
+    if (isFirstInRange) {
+      lastInRange = currentNumber + 1;
 
-      while(numberTracker[current]) {
-        current++;
+      while(tracker[lastInRange]) {
+        lastInRange++;
       }
 
-      const lengthLongestRange = longestRange[1] - longestRange[0];
+      const longestRangeLength = longestRange[1] - longestRange[0];
 
-      if (current - numberTracker[num] > lengthLongestRange) {
-        longestRange = [numberTracker[num], current - 1];
+      if (lastInRange - currentNumber > longestRangeLength) {
+        longestRange = [currentNumber, lastInRange - 1];
       }
     }
   }
